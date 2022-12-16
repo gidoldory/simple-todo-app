1a8f2638aa1ffa5c54cf349f61e14edb501ea498
gidoldory <gidol79@gmail.com>
2022-12-16 23:15:23 +0900

    feat: eslint, prettier 설정

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import styled from "styled-components";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./redux/counterSlice";
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CountButton = styled.button`
+  width: 50px;
+  height: 50px;
+  font-size: 2rem;
+  font-weight: bold;
+  color: white;
+  background-color: ${(props) => props.color};
+`;
 
 function App() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{count}</p>
+        <ButtonContainer>
+          <CountButton color="red" onClick={() => dispatch(decrement())}>
+            -
+          </CountButton>
+          &nbsp;
+          <CountButton color="blue" onClick={() => dispatch(increment())}>
+            +
+          </CountButton>
+        </ButtonContainer>
       </header>
     </div>
   );
